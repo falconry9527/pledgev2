8d6c7beb6fe151a7ddd1cddd81411d2878ccb249
conry <kequan888@gmail.com>
2025-08-31 17:04:55 +0800

    依赖调整

--- contracts/pledge/BscPledgeOracle.sol
+++ contracts/pledge/BscPledgeOracle.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.19;
+pragma solidity 0.6.12;
 
 import "../multiSignature/multiSignatureClient.sol";
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
--- contracts/pledge/PledgePool.sol
+++ contracts/pledge/PledgePool.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.19;
+pragma solidity 0.6.12;
 
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "../library/SafeTransfer.sol";
